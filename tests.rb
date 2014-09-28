e62e7f9f6570036ec248da7922a1c0c11ee0658a
Bao Lei <bao@vessel.com>
2014-09-27 20:59:16 -0700

    Updated unit test

--- tests.rb
+++ tests.rb
@@ -1,76 +1,32 @@
-require './vfl2objc.rb'
-require 'test/unit'
+require './vfl2code.rb'
+require 'minitest/autorun'
 
-class VFLTest < Test::Unit::TestCase
+class VFLTest < Minitest::Test
     def test_basic
         input = "|[b1]|"
-        output = %Q{
-frame = b1.frame;
-frame.origin.x = 0;
-frame.size.width = superview.bounds.size.width - (0) - frame.origin.x;
-// You need to set frame.size.height
-// You need to set frame.origin.y
-b1.frame = frame;
-b1.autoresizingMask |= UIViewAutoresizingFlexibleWidth;
-// You need to figure out the vertical autoresizing mask
-[superview addSubview:b1];
-}
-        assert str2str(input).include? output
+        output = "CGRect f0;f0=b1.frame;f0.origin.x=0;f0.size.width=superview.bounds.size.width-(0)-f0.origin.x;b1.frame=f0;b1.autoresizingMask|=UIViewAutoresizingFlexibleWidth;[superview addSubview:b1];"
+        assert transform_raw_code(input, false, 0).include? output
     end
 
 
     def test_predefined_size
         input = "V:|-x-[itemA(w)]-x2-[itemB]-x3-[itemC(>0)]|"
-        output = %Q{
-frame = itemA.frame;
-// You need to set frame.size.width
-// You need to set frame.origin.x
-frame.origin.y = 0 + x;
-frame.size.height = w;
-itemA.frame = frame;
-// You need to figure out the horizontal autoresizing mask
-itemA.autoresizingMask |= UIViewAutoresizingFlexibleBottomMargin;
-[superview addSubview:itemA];
+        output = "CGRect f0;f0=itemA.frame;f0.origin.y=0+x;f0.size.height=w;itemA.frame=f0;itemA.autoresizingMask|=UIViewAutoresizingFlexibleBottomMargin;[superview addSubview:itemA];f0=itemB.frame;f0.origin.y=0+x+w+x2;itemB.frame=f0;itemB.autoresizingMask|=UIViewAutoresizingFlexibleBottomMargin;[superview addSubview:itemB];f0=itemC.frame;f0.origin.y=CGRectGetMaxY(itemB.frame)+0+x3;f0.size.height=superview.bounds.size.height-(0)-f0.origin.y;itemC.frame=f0;itemC.autoresizingMask|=UIViewAutoresizingFlexibleHeight;[superview addSubview:itemC];"
 
-frame = itemB.frame;
-// You need to set frame.size.width
-// You need to set frame.origin.x
-frame.origin.y = 0 + x + w + x2;
-// You need to set frame.size.height
-itemB.frame = frame;
-// You need to figure out the horizontal autoresizing mask
-itemB.autoresizingMask |= UIViewAutoresizingFlexibleBottomMargin;
-[superview addSubview:itemB];
-
-frame = itemC.frame;
-// You need to set frame.size.width
-// You need to set frame.origin.x
-frame.origin.y = CGRectGetMaxY(itemB.frame) + 0 + x3;
-frame.size.height = superview.bounds.size.height - (0) - frame.origin.y;
-itemC.frame = frame;
-// You need to figure out the horizontal autoresizing mask
-itemC.autoresizingMask |= UIViewAutoresizingFlexibleHeight;
-[superview addSubview:itemC];
-}
-        assert str2str(input).include? output
+        assert transform_raw_code(input, false, 0).include? output
     end
 
 
     def test_incomplete
         input = "[d(40)]"
-        output = %Q{
-frame = d.frame;
-frame.size.width = 40;
-// You need to set frame.origin.x
-// You need to set frame.size.height
-// You need to set frame.origin.y
-d.frame = frame;
-// You need to figure out the horizontal autoresizing mask
-// You need to figure out the vertical autoresizing mask
-[superview addSubview:d];
-}
-        assert str2str(input).include? output
+        output = "CGRect f0;f0=d.frame;f0.size.width=40;d.frame=f0;[superview addSubview:d];"
+        assert transform_raw_code(input, false, 0).include? output
     end
 
+    def test_swift
+      input = "center[e(50)]\nV:[e(50)]-30-|"
+      output = "var f0:CGRect;f0=e.frame;f0.size.width=50;f0.origin.x=(superview.bounds.size.width-f0.size.width)/2.0;f0.origin.y=superview.bounds.size.height-(0+30)-50;f0.size.height=50;e.frame=f0;e.autoresizingMask|=UIViewAutoresizing.FlexibleLeftMargin|UIViewAutoresizing.FlexibleRightMargin;e.autoresizingMask|=UIViewAutoresizing.FlexibleTopMargin;superview.addSubview(e);"
+      assert transform_raw_code(input, true, 0).include? output
+    end
 
 end
